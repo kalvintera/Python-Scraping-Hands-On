7bc8e6aae8703c423de595c6e9152c3ab57d7c5b
kalvintera <irem.ebenstein@steincode.com>
2024-12-22 22:04:25 +0100

    fix: improved logger

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-import logging
+import sys
+from loguru import logger
 from dataclasses import dataclass
 from pathlib import Path
 from fake_useragent import UserAgent
@@ -23,11 +24,14 @@ class Config:
     """
 
     project_path: Path = Path(__file__).parent.resolve()
+
     urls_csv_path: Path = project_path.joinpath("urls.csv")
     urls_json_path: Path = project_path.joinpath("urls.json")
     output_path: Path = project_path.joinpath("output")
+    print(output_path)
     delimiter: str = ";"
     encoding: str = "utf-8"
+    start_headless = False
 
     boolean_cols = ["selenium", "pandas", "bs4", "newspaper3K", "paginated"]
 
@@ -37,24 +41,53 @@ class Config:
     }
 
     @staticmethod
-    def initialize_logger(package: str = None):
+    def initialize_logger() -> None:
         """
-        Initialisiert und konfiguriert den Logger für das angegebene Paket oder das Root-Logger.
+        Initialisiert und konfiguriert den Logger.
 
-        Args:
-            package (str, optional): Der Name des Pakets, für das der Logger konfiguriert werden soll.
-                                     Falls None, wird der Root-Logger konfiguriert.
         """
-        logger = logging.getLogger(package) if package else logging.getLogger()
-        logger.setLevel(logging.DEBUG)
 
-        # Stellt sicher, dass kein doppelter Handler hinzugefügt wird.
-        if not logger.handlers:
-            # Erstellt einen StreamHandler, der die Log-Nachrichten in der Konsole ausgibt.
-            stream_handler = logging.StreamHandler()
-            stream_handler.setFormatter(
-                logging.Formatter(
-                    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-                )
-            )
-            logger.addHandler(stream_handler)
+        # Für mehr Info: https://loguru.readthedocs.io/en/stable/api/logger.html
+        log_level = "DEBUG"
+        log_format = ("<green>{time:YYYY-MM-DD HH:mm:ss.SSS zz}</green> | <level>{level: <8}</level> | <yellow>Line "
+                      "{line: >4} ({file}):</yellow> <b>{message}</b>")
+        logger.add(sys.stderr, level=log_level, format=log_format, colorize=True, backtrace=True, diagnose=True)
+
+        # spezifische Handler für Fehler und kritische Stufen mit benutzerdefinierten:
+        logger.add(
+            sys.stderr,
+            level="ERROR",
+            format="<red>{time:YYYY-MM-DD HH:mm:ss.SSS zz}</red> | <level>{level: <8}</level> | <red>Line {line: >4} "
+                   "({file}):</red> <level>{message}</level>",
+            colorize=True,
+            backtrace=True,
+            diagnose=True
+        )
+
+        # Warnmeldungen mit Gelb einfärben
+        log_format_warning = (
+            "<yellow>{time:YYYY-MM-DD HH:mm:ss.SSS zz}</yellow> | "
+            "<yellow>{level: <8}</yellow> | "
+            "<yellow>Line {line: >4} ({file}):</yellow> "
+            "<yellow>{message}</yellow>"
+        )
+
+        # sys.stderr -> logging für die Konsole
+        logger.add(
+            sys.stderr,
+            level="WARNING",
+            format=log_format_warning,
+            colorize=True,
+            backtrace=True,
+            diagnose=True
+        )
+        logger.add(
+            sys.stderr,
+            level="CRITICAL",
+            format="<bold red>{time:YYYY-MM-DD HH:mm:ss.SSS zz}</bold red> | <level>{level: <8}</level> | "
+                   "<bold red>Line {line: >4} ({file}):</bold red> <level>{message}</level>",
+            colorize=True,
+            backtrace=True,
+            diagnose=True
+        )
+
